7337bf94c617a04c3a1f3b454c9c001313d1d123
hyunkyungYu <youhk43@naver.com>
2024-01-22 13:54:42 +0900

    product data 그룹핑

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import "./App.css";
+import product from "./data/product.json"
+import { useState } from 'react';
+
+function FilterableProductTable() {
+ return; 
+}
+
+function SearchBar() {
+  return;
+}
+
+function ProductTable() {
+  return;
+}
+
+function ProductCategoryRow() {
+  return;
+}
+
+function ProductRow() {
+  const result = product.reduce((acc, curr) => {  
+    const { category } = curr;                       
+    if (acc[category]) acc[category].push(curr);  
+    else acc[category] = [curr];             
+    return acc;                         
+  }, {});
+
+  console.log(result);
+  
+  return (Object.keys(result).map((cate, index) => {
+   <li key={index}>
+      {cate}
+    </li>
+  }));
+}
 
 function App() {
   return (
     <div className="App">
-      <div className="container">{/* 코드 추가 test*/}</div>;
+      <div className="container">
+        <ProductRow />
+      </div>;
     </div>
   );
 }
