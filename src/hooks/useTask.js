c43391a7a3e2aebea7912d2b4838fcc1e7c3496e
wonmoo <hwmkmh@gmail.com>
2024-01-27 20:20:22 +0900

    task 상태 관리를 위해 useReducer 사용

--- src/hooks/useTask.js
+++ src/hooks/useTask.js
@@ -1,33 +1,66 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const ADD_TASK = "ADD_TASK";
+const UPDATE_TASK = "UPDATE_TASK";
+const DELETE_TASK = "DELETE_TASK";
 
 export default function useTask(initialTasks) {
-  const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+  function nextId() {
+    if (tasks.length === 0) return 0;
+    const maxId = tasks.reduce((max, task) => Math.max(max, task.id), -1);
+    return maxId + 1;
+  }
+
+  function tasksReducer(tasks, action) {
+    switch (action.type) {
+      case ADD_TASK:
+        return [...tasks, action.task];
+      case UPDATE_TASK:
+        return tasks.map((t) => {
+          if (t.id === action.task.id) {
+            return action.task;
+          } else {
+            return t;
+          }
+        });
+      case DELETE_TASK:
+        return tasks.filter((t) => t.id !== action.id);
+      default:
+        throw Error("Unknown action: " + action.type);
+    }
+  }
+
   function addTask(text) {
-    setTasks((prevTasks) => {
-      const lastId =
-        prevTasks.length > 0 ? prevTasks[prevTasks.length - 1].id : -1;
-      const newTask = { id: lastId + 1, text, done: false };
-      return [...prevTasks, newTask];
+    dispatch({
+      type: ADD_TASK,
+      task: {
+        id: nextId(),
+        text,
+        done: false,
+      },
     });
   }
 
   function deleteTask(id) {
-    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+    dispatch({
+      type: DELETE_TASK,
+      id,
+    });
   }
 
   function updateTask(task) {
-    setTasks((prevTasks) => {
-      const index = prevTasks.findIndex((t) => t.id === task.id);
-      const newTasks = [...prevTasks];
-      newTasks[index] = task;
-      return newTasks;
+    dispatch({
+      type: UPDATE_TASK,
+      task,
     });
   }
 
   return {
     tasks,
     addTask,
-    deleteTask,
     updateTask,
+    deleteTask,
   };
 }
