fa38c218071afb14500545f86834c2ac4427178b
wonmoo <hwmkmh@gmail.com>
2024-01-27 09:16:50 +0900

    5단계: 이벤트 핸들러를 연결하여 상태 설정

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import "./Quiz.css";
 import Spinner from "./Spinner.js";
 
-export default function Quiz({ status = "error" }) {
-  const isEmpty = status === "empty";
-  // const isTyping = status === "typing";
-  const isSubmitting = status === "submitting";
-  const isError = status === "error";
-  const isSuccess = status === "success";
+export default function Quiz() {
+  //
+  const [answer, setAnswer] = useState("");
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("typing"); // 'typing' | 'submitting' | 'success'
 
-  if (isSuccess) {
+  if (status === "success") {
     return <h2>Thank you</h2>;
+  }
+
+  const isSubmitting = status === "submitting";
+  const isError = error !== null;
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("submitting");
+    try {
+      await submitForm(answer);
+      setStatus("success");
+    } catch (error) {
+      setStatus("typing");
+      setError(error.message);
+    }
   }
 
   return (
@@ -19,14 +34,30 @@ export default function Quiz({ status = "error" }) {
       <p>
         In which city is there a billboard that turns air into drinkable water?
       </p>
-      <form>
-        <textarea disabled={isSubmitting} />
+      <form onSubmit={handleSubmit}>
+        <textarea
+          disabled={isSubmitting}
+          value={answer}
+          onChange={(e) => setAnswer(e.target.value)}
+        />
         <br />
-        <button disabled={isEmpty || isSubmitting}>Submit</button>
-        {isError && (
-          <p className="error">Good guess but a wrong answer. Try again!</p>
-        )}
+        <button disabled={answer.trim().length === 0 || isSubmitting}>
+          Submit
+        </button>
+        {isError && <p className="error">{error}</p>}
       </form>
     </>
   );
+}
+
+function submitForm(answer) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (answer.toLowerCase() === "lima") {
+        resolve();
+      }
+
+      reject(new Error("Good guess but a wrong answer. Try again!"));
+    }, 1000);
+  });
 }
