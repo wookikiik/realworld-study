71a44c1aa77a6727b8d8a7366b866511e9123824
wonmoo <hwmkmh@gmail.com>
2024-02-29 14:17:19 +0900

    Refactor authentication pages

--- app/(pages)/(authentication)/register/page.tsx
+++ app/(pages)/(authentication)/register/page.tsx
@@ -3,46 +3,40 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="auth-page">
-      <div className="container page">
-        <div className="row">
-          <div className="col-md-6 offset-md-3 col-xs-12">
-            <h1 className="text-xs-center">Sign up</h1>
-            <p className="text-xs-center">
-              <Link href="/login">Have an account?</Link>
-            </p>
+    <>
+      <h1 className="text-xs-center">Sign up</h1>
+      <p className="text-xs-center">
+        <Link href="/login">Have an account?</Link>
+      </p>
 
-            <ErrorMessages messages={["That email is already taken"]} />
+      <ErrorMessages messages={["That email is already taken"]} />
 
-            <form>
-              <fieldset className="form-group">
-                <input
-                  className="form-control form-control-lg"
-                  type="text"
-                  placeholder="Username"
-                />
-              </fieldset>
-              <fieldset className="form-group">
-                <input
-                  className="form-control form-control-lg"
-                  type="text"
-                  placeholder="Email"
-                />
-              </fieldset>
-              <fieldset className="form-group">
-                <input
-                  className="form-control form-control-lg"
-                  type="password"
-                  placeholder="Password"
-                />
-              </fieldset>
-              <button className="btn btn-lg btn-primary pull-xs-right">
-                Sign up
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+      <form>
+        <fieldset className="form-group">
+          <input
+            className="form-control form-control-lg"
+            type="text"
+            placeholder="Username"
+          />
+        </fieldset>
+        <fieldset className="form-group">
+          <input
+            className="form-control form-control-lg"
+            type="text"
+            placeholder="Email"
+          />
+        </fieldset>
+        <fieldset className="form-group">
+          <input
+            className="form-control form-control-lg"
+            type="password"
+            placeholder="Password"
+          />
+        </fieldset>
+        <button className="btn btn-lg btn-primary pull-xs-right">
+          Sign up
+        </button>
+      </form>
+    </>
   );
 }
