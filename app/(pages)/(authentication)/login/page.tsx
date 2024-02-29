e73a5d43697491e1ceb429ece3e2bb734b12a768
wonmoo <hwmkmh@gmail.com>
2024-02-29 20:51:31 +0900

    Add form validation using zod and @hookform/resolvers

--- app/(pages)/(authentication)/login/page.tsx
+++ app/(pages)/(authentication)/login/page.tsx
@@ -6,14 +6,27 @@ import { signIn } from "@/app/lib/actions";
 import { SigninForm } from "@/app/lib/definitions";
 import { normalizeFormErrors } from "@/app/lib/utils";
 import { ErrorMessages } from "@/app/ui/components";
-import { EmailInput, PasswordInput } from "../_lib/fields";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+
+const schema = z
+  .object({
+    email: z.string().email("Email must be a valid email address"),
+    password: z
+      .string()
+      .trim()
+      .min(5, "Password must be at least 5 characters long"),
+  })
+  .required();
 
 export default function Page() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<SigninForm>();
+  } = useForm<SigninForm>({
+    resolver: zodResolver(schema),
+  });
 
   const actionSignIn: () => void = handleSubmit(async (data: SigninForm) => {
     await signIn(data);
@@ -30,10 +43,22 @@ export default function Page() {
 
       <form onSubmit={actionSignIn}>
         <fieldset className="form-group">
-          <EmailInput label="email" register={register} />
+          <input
+            className="form-control form-control-lg"
+            type="text"
+            placeholder="Email"
+            required
+            {...register("email")}
+          />
         </fieldset>
         <fieldset className="form-group">
-          <PasswordInput label="password" register={register} />
+          <input
+            className="form-control form-control-lg"
+            type="password"
+            placeholder="Password"
+            required
+            {...register("password")}
+          />
         </fieldset>
         <button type="submit" className="btn btn-lg btn-primary pull-xs-right">
           Sign in
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,17 +1,26 @@
 "use server";
 
-import { signIn as authSignIn } from "@/auth";
+import { signIn as authentication } from "@/auth";
 
-import { SigninForm } from "./definitions";
+import { SigninForm, SignupForm } from "./definitions";
 import { AuthError } from "next-auth";
 
-export async function signIn(formData: SigninForm) {
+export async function signIn(formData: SigninForm): Promise<string | void> {
   try {
-    await authSignIn("credentials", formData);
+    await authentication("credentials", formData);
   } catch (error) {
     if (error instanceof AuthError) {
-      // TODO: handle error
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
     }
     throw error;
   }
+}
+
+export async function signUp(formData: SignupForm) {
+  //
 }
