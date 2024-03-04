60156adfc39d9ab6468e1523ccdc56e7dfe385be
hyunkyungYu <youhk43@naver.com>
2024-03-05 00:23:25 +0900

    login에 react hook form 적용중

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,22 @@
+'use client'
+import Link from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form"
+import UserInfo from "../ui/userInfo";
+
+type Inputs = {
+  email: string
+  password: string
+}
+
 export default function Page() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
   return (
     <div className="auth-page">
       <div className="container page">
@@ -6,20 +24,25 @@ export default function Page() {
           <div className="col-md-6 offset-md-3 col-xs-12">
             <h1 className="text-xs-center">Sign in</h1>
             <p className="text-xs-center">
-              <a href="/register">Need an account?</a>
+              <Link href="/register">Need an account?</Link>
             </p>
 
-            <ul className="error-messages">
-              <li>That email is already taken</li>
-            </ul>
-
-            <form>
-              <fieldset className="form-group">
-                <input className="form-control form-control-lg" type="text" placeholder="Email" />
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <ul className="error-messages">
+                {errors && Object.entries(errors).map(([key, error]) => (
+                  <li key={key}>{error.message}</li>
+                ))}
+              </ul>
+              <UserInfo />
+              {/* <fieldset className="form-group">
+                <input className="form-control form-control-lg"
+                  type="text" placeholder="Email"
+                  {...register("email", { required: "email required" })} />
               </fieldset>
               <fieldset className="form-group">
-                <input className="form-control form-control-lg" type="password" placeholder="Password" />
-              </fieldset>
+                <input className="form-control form-control-lg" type="password" placeholder="Password"
+                  {...register("password", { required: "password required" })} />
+              </fieldset> */}
               <button className="btn btn-lg btn-primary pull-xs-right">Sign in</button>
             </form>
           </div>
