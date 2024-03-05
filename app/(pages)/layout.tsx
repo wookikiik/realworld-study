11856f19f484f790413882fc20d0ba9d12e9049b
wonmoo <hwmkmh@gmail.com>
2024-03-05 20:09:10 +0900

    Update font family in layout and global CSS files

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${source_sans.className}`}>
+      <body className={`${source_sans.variable}`}>
         <main>
           <UserProvider>
             <Navigation />
